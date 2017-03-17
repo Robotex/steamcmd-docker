1b5c0097bb157e7e35e6f949c10fc5e579e6ded6
Robotex <robotex@altervista.org>
2017-03-17 17:31:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,7 @@
 if [ ! -f /usr/games/gameserver/serverfiles/srcds_run ]
 then
     LOCKDIR="/usr/games/gameserver/serverfiles"
-    flock -xn $LOCKDIR
-    if [ $? -eq 0 ]
-    then
-        echo "Master update..."
-        while [ "${exitcode}" != "0" ]
-        do
-            /usr/games/steamcmd +runscript /usr/games/gameserver/update.txt
-            exitcode=$?
-        done
-    else
-        echo "Slave waiting..."
-        flock -x $LOCKDIR
-    fi
-    flock -u 
+    flock -xn $LOCKDIR -c while true; do /usr/games/steamcmd +runscript /usr/games/gameserver/update.txt done || flock -u $LOCKDIR
 fi
 
 . /usr/games/gameserver/start.sh
