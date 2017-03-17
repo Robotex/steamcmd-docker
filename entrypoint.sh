7ec2839d4a0d837c87f47d50d63f815196ae87da
Robotex <robotex@altervista.org>
2017-03-17 20:06:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ ! -f /usr/games/gameserver/serverfiles/srcds_run ]
 then
     LOCKDIR="/usr/games/gameserver/serverfiles"
-    flock -xn $LOCKDIR -c 'while sleep 5; do /usr/games/steamcmd +runscript /usr/games/gameserver/update.txt; done' || flock -x $LOCKDIR -c ':'
+    flock -xn $LOCKDIR -c 'while sleep 5; do /usr/games/steamcmd +runscript /usr/games/gameserver/update.txt && break; done' || flock -x $LOCKDIR -c ':'
 fi
 
 . /usr/games/gameserver/start.sh
